d34a0fa6815ee86fe1f539f82def7df38b59e367
ssingh <ssingh@impetus.com>
2025-11-14 10:36:56 -0500

    hasshing load balancer: Fix key migration on server add/remove

--- consistent_hashing_load_balancer/consistant_hashing.py
+++ consistent_hashing_load_balancer/consistant_hashing.py
@@ -45,45 +45,46 @@ class ConsistentHashing:
         self.server_hashes = {}  # server -> list of hashes
         self.servers = {}  # server -> list of hashes
 
-    def add_server(self, server):
-        """Add a server to the hash ring and identify keys that need to move."""
-        if server in self.server_hashes:
-            return
-
-        self.server_hashes[server] = []
-        # number of virtual nodes per server.
-        # each virtual node will be added to the ring
-        ring_extens = []
-        server_node = Server(server)
-        for i in range(self.num_replicas):
-            vName_name = f"{server}:{i}"
+    def _generate_unique_hash(self, serverName, vIndex,vName_name):
             hash_val = Utils.hash_key(vName_name)
             j=1
             while hash_val in [item[0] for item in self.ring]:
-                vName_name = f"{server}:{i}_{j}"
+                vName_name = f"{serverName}:{vIndex}_{j}"
                 hash_val = Utils.hash_key(vName_name)
                 j+=1
+            return hash_val
+
+    def add_server(self, server_name):
+        """Add a server to the hash ring and identify keys that need to move."""
+        if server_name in self.server_hashes:
+            return
+
+        self.server_hashes[server_name] = []
+        # number of virtual nodes per server.
+        # each virtual node will be added to the ring
+        ring_extens = []
+        server_node = Server(server_name)
+        for i in range(self.num_replicas):
+            vName_name = f"{server_name}:{i}"
+            hash_val = self._generate_unique_hash(server_name, i, vName_name)
             vName_name = f"{vName_name}:{hash_val}"
-            self.server_hashes[server].append(hash_val)
-            self.servers[server] = server_node
+            self.server_hashes[server_name].append(hash_val)
+            self.servers[server_name] = server_node
             server_node.add_vnode(hash_val)
-            ring_extens.append((hash_val, server, vName_name))
+            ring_extens.append((hash_val, server_name, vName_name))
 
         for ring_extend_item in ring_extens:
-            #print(" added in ring : ", ring_extend_item)
             bisect.insort(self.ring, ring_extend_item)
-        #print(f"Added server: {server}")
-        #print("\n".join([str(item) for item in self.ring]))
 
         if len(self.ring) == len(ring_extens):
             #print("First server added, no keys to move.")
             return
 
-        # Move   keys that now belong to the new server
+        # Move  keys that now belong to the new server
         new_vnode_hashs = [item[0] for item in ring_extens]
         if ring_extens:
             for ring_extend_item in ring_extens:
-                hash_val, server, vName_name = ring_extend_item
+                hash_val, server_name, vName_name = ring_extend_item
 
                 current_vnode  , current_index = self.get_server_by_hash(hash_val)
                 vnode  , data_server_index = self.get_server_by_hash(hash_val-1)
@@ -112,53 +113,56 @@ class ConsistentHashing:
                         if (hash_val <= key_hash or key_hash < right_vnode_hash) :
                             keyes_moved.add(key)
 
+                #self.__move_all_keys(data_server_index, current_index, keyes_moved)
+                
                 if keyes_moved:
                     for key in keyes_moved:
                         value = data_server_node.get(data_server_start_hash,key)
                         data_server_node.delete(data_server_start_hash,key)
                         server_node.add(hash_val, key, value)
-
-    def remove_server(self, server):
+            
+    def remove_server(self, server_name):
         """Remove a server from the hash ring and identify keys that need to move."""
-        if server not in self.server_hashes:
+        if server_name not in self.server_hashes:
             return
 
-        for hash_val in self.server_hashes[server]:
-            current_vnode , index  = self.get_server_by_hash(hash_val)
-            light_index = (index -1) % len(self.ring)
-            while self.ring[light_index][0] in self.server_hashes[server]:
-                if light_index == 0:
-                    light_index = len(self.ring) -1
+        for hash_val in self.server_hashes[server_name]:
+            # virtual node being removed
+            current_vnode , old_index  = self.get_server_by_hash(hash_val)
+            
+            # Find the left next server in the ring that is not being removed
+            new_index = (old_index -1) % len(self.ring)
+            while self.ring[new_index][0] in self.server_hashes[server_name]:
+                if new_index == 0:
+                    new_index = len(self.ring) -1
                 else:
-                    light_index = (light_index - 1) % len(self.ring)
+                    new_index = (new_index - 1) % len(self.ring)
+            # Move keys to the next server in the ring
+            self.__move_all_keys(old_index, new_index)
 
-            keys_to_move = []
-            next_vnode = self.ring[light_index]
-            next_server = next_vnode[1]
-            removed_server_node = self.servers[server]
-            newowner_server_node = self.servers[next_server]
-
-            for key_hash in removed_server_node.data[current_vnode[0]].keys():
-                keys_to_move.append(key_hash)
-
-            for key in keys_to_move:
-                value = removed_server_node.get(current_vnode[0], key)
-                newowner_server_node.add(next_vnode[0], key, value)
-                removed_server_node.delete(current_vnode[0], key)
-                #print( f"  Moved key '{key}' from {current_vnode} to {next_vnode}")
-
-        # Move keys to the next server in the ring
-        for hash_val in self.server_hashes[server]:
-            # Find and remove from ring
-            for i, (hash_val, server_name, vName_name) in enumerate(self.ring):
-                if hash_val == hash_val and server_name == server:
+        for hash_val in self.server_hashes[server_name]:
+            # Find vNode and remove from ring
+            for i, (hash_val, s_name, v_name) in enumerate(self.ring):
+                if hash_val == hash_val and server_name == s_name:
                     del self.ring[i]
                     break
-
         #print(f"Removed server: {server}")
-        del self.server_hashes[server]
-        del self.servers[server]
+        del self.server_hashes[server_name]
+        del self.servers[server_name]
 
+    def __move_all_keys(self, old_index, new_index, keys_to_move = None):
+        old_vnode = self.ring[old_index]
+        next_vnode = self.ring[new_index]
+        old_server_node = self.servers[old_vnode[1]]
+        new_server_node = self.servers[next_vnode[1]]
+        if not keys_to_move:
+            keys_to_move = list(self.servers[old_vnode[1]].data[old_vnode[0]].keys())
+        if keys_to_move:
+            for key in keys_to_move:
+                value = old_server_node.get(old_vnode[0], key)
+                new_server_node.add(next_vnode[0], key, value)
+                old_server_node.delete(old_vnode[0], key)
+                #print( f"  Moved key '{key}' from {current_vnode} to {next_vnode}")
 
     def get_server(self, key):
         """Get the server responsible for the key."""
